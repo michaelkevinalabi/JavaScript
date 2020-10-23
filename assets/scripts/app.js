a544d8cb2ebab350432d1154e35d19ce547963b6
michaelkevinalabi <michael.alabi-duobu@amalitech.org>
2020-10-23 22:10:15 +0000

    using constants

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,7 +1,8 @@
+const defaultResult = 0;
 let currentResult = 0;
 
 currentResult = currentResult + 10 * 3 / 2 - 3; 
 
-let calculationDescription = '(' + currentResult + ' + 10 * 3 / 2 - 3)';
+let calculationDescription = '(' + defaultResult + ' + 10 * 3 / 2 - 3)';
 
 outputResult(currentResult, calculationDescription)
