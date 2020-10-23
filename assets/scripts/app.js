5d6c4bda911c0e88c6f9ea7cf5c61c80c6dc4e52
michaelkevinalabi <michael.alabi-duobu@amalitech.org>
2020-10-23 23:52:15 +0000

    adding a custom function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,8 +1,15 @@
 const defaultResult = 0;
-let currentResult = 0;
+let currentResult = defaultResult;
+
+function add(number1, number2) {
+    const result = number1 + number2;
+    alert('The result is ' + result);
+}
+
+add(4 , 5);
 
 currentResult = currentResult + 10 * 3 / 2 - 3; 
 
 let calculationDescription = '(' + defaultResult + ' + 10 * 3 / 2 - 3)';
 
-outputResult(currentResult, calculationDescription)
+outputResult(currentResult, calculationDescription);
