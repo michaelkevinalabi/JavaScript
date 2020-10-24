9c45c9a19aa7b1e97797f6a0fed3d1d96963e390
michaelkevinalabi <michael.alabi-duobu@amalitech.org>
2020-10-24 22:17:24 +0000

    splitting code into functions

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,7 +1,12 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+function getUserNumberInput(){
+    return parseInt(usrInput.value);
+}
+
 function add() {
+    const enteredNumber = getUserNumberInput();
     const calcDescription = `${currentResult} + ${userInput.value}`;
     currentResult = currentResult + parseInt(userInput.value);
     outputResult(currentResult, calcDescription);
