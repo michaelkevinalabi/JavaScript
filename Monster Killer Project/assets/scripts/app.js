2342360c3202fe71d9c7cf91dc881d1768d11a1a
michaelkevinalabi <michael.alabi-duobu@amalitech.org>
2020-10-28 23:28:14 +0000

    heal player

--- Monster Killer Project/assets/scripts/app.js
+++ Monster Killer Project/assets/scripts/app.js
@@ -1,6 +1,7 @@
 const ATTACK_VALUE = 10;
 const STRONG_ATTACK_VALUE = 20;
 const MONSTER_ATTACK_VALUE = 14;
+const HEAL_VALUE = 20; 
 
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
@@ -36,5 +37,10 @@ function strongAttackHandler() {
     attackMonster('STRONG_ATTACK');
 }
 
+function healPlayerHandler() {
+    
+}
+
 attackBtn.addEventListener('click', attackHandler);
 strongAttackBtn.addEventListener('click', strongAttackHandler);
+healBtn.addEventListener('click');
